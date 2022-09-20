d9d477396147a81f21dc5c06363626f263a09c58
Ruben Perez <rubenperez@Rubens-MacBook-Pro.local>
2022-09-20 13:12:10 -0400

    learn about numbers

---
+++ numbers.php
@@ -0,0 +1,33 @@
+<?php 
+	//interger
+	$number = 85;
+	//float
+	$otherNumber = 3.56;
+
+	//increment & decrement operators
+	$number++;
+	$number--;
+
+	//shorthand operators
+	$number += 80;
+	$number -= 10;
+	$number *= 2;
+
+	echo floor($otherNumber);
+	echo ceil($otherNumber);
+	echo pi()
+
+	
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Numbers</title>
+</head>
+<body>
+	<h1><?php echo $number ?></h1> 	
+</body>
+</html>
