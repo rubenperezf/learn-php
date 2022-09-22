a4a8dc4c909def7a97099db4f9d645471eaf0686
Ruben Perez <rubenperez@Rubens-MacBook-Pro.local>
2022-09-22 09:22:19 -0400

    add some arrays examples

--- arrays.php
+++ arrays.php
@@ -1,11 +1,21 @@
 <?php 
-	//interger
-	$arrayOne = ["a"];
-
-
+	
+	//indexed arrays
+	$arrayOne = ["a", "b", "c"];
+	$arrayTwo = array(1, 2, 3);
+	$arrayOne[1] = "z";
+	$arrayOne[] = "adios";
+	array_push($arrayOne, "hola");
+	$mergedArray = array_merge($arrayOne, $arrayTwo);
 	
 
+	//associative arrays
+	//key value pairs
 
+	$arrayThree = ["shaun" => "black", "mario" => "orange", "luigi" => "brown"];
+	$arrayFour = array("bower" => "green", "peach" => "yellow");
+	$arrayFour["toad"] = "pink";
+	$mergedArray2 = array_merge($arrayThree, $arrayFour);
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +24,16 @@
 	<title>Arrays</title>
 </head>
 <body>
-	<h1><?php print_r($arrayOne) ?></h1> 	
+	<h1><?php print_r($arrayOne) ?></h1>
+	<h1><?php echo $arrayOne[0] ?></h1> 	
+	<h1><?php echo $arrayTwo[0] ?></h1> 	
+	<h1><?php echo count($arrayOne) ?></h1> 	
+	<h1><?php print_r($mergedArray) ?></h1> 	
+	<h1><?php print_r($arrayThree) ?></h1> 	
+	<h1><?php echo $arrayThree["mario"] ?></h1> 	
+	<h1><?php print_r($arrayFour) ?></h1> 	
+	<h1><?php echo count($arrayFour) ?></h1> 
+	<h1><?php print_r($mergedArray2) ?></h1> 
+		
 </body>
 </html>
